f474309c62576a43db7c9bb50649825516c78041
Alex Harris <alexharrisemail@gmail.com>
2023-08-16 08:56:43 -0400

    adds better filtering so that list of data will always be filtered by original list

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            monsters: []
+            monsters: [],
+            searchField: ''
         };
     }
 
@@ -21,6 +22,13 @@ class App extends Component {
     }
 
     render() {
+        const filteredMonsters = this
+        .state
+        .monsters
+        .filter((monster) =>{
+            return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+        } 
+        );
         return (
             <div className="App">
                 <input
@@ -28,21 +36,14 @@ class App extends Component {
                     type='search'
                     placeholder='search monsters'
                     onChange={(event) => {
-                    console.log(event.target.value)
-                    const searchString = event.target.value.toLocaleLowerCase();
-                    const filteredMonsters = this
-                        .state
-                        .monsters
-                        .filter((monster) =>{
-                            return monster.name.toLocaleLowerCase().includes(searchString);
-                        } 
-                        );
+                    
+                    const searchField = event.target.value.toLocaleLowerCase();
+                 
                         this.setState(() => {
-                            return {monsters: filteredMonsters }
+                            return {searchField}
                         })
-                }}/> {this
-                    .state
-                    .monsters
+                }}/> {
+                    filteredMonsters
                     .map((monster) => {
                         return <div key={monster.id}>
                             <h1>{monster.name}</h1>
